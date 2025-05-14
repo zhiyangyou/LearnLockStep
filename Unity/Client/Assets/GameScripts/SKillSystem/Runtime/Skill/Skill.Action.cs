f246940760834c99f4209d1d4e45d767cda58615
youzhiyang <1454900087@qq.com>
2025-05-14 15:01:26 +0800

    NEW: 贝塞尔曲线手雷

--- Unity/Client/Assets/GameScripts/SKillSystem/Runtime/Skill/Skill.Action.cs
+++ Unity/Client/Assets/GameScripts/SKillSystem/Runtime/Skill/Skill.Action.cs
@@ -15,90 +15,98 @@ public partial class Skill {
     }
 
     public void AddMoveAction(SkillConfig_Action configAction, LogicObject actionActor, Vector3 effectOffset, Action onMoveFinish, Action onMoveUpdate) {
-        FixIntVector3 movePos = new FixIntVector3(configAction.movePos);
-        FixIntVector3 targetPos = actionActor.LogicPos + movePos * actionActor.LogicAxis_X;
-        FixIntVector3 startPos = actionActor.LogicPos;
-        // Debug.LogError($"add move action pos:{targetPos}");
-        MoveType moveType = MoveType.Target;
-        +
-        if (configAction.moveActionType == MoveActionType.TargerPos) {
-            if (movePos.IsOnlyAxis_X()) {
-                moveType = MoveType.X;
+        void OnActionFinish() {
+            onMoveFinish?.Invoke();
+            switch (configAction.moveActionFinishOpation) {
+                case MoveActionFinishOpation.None:
+                    break;
+                case MoveActionFinishOpation.Skill: {
+                    foreach (int skillID in configAction.actionFinishIDList) {
+                        _skillCreater.ReleaseSkill(skillID, null);
+                    }
+                }
+                    break;
+                case MoveActionFinishOpation.Buff: {
+                    // buff系统
+                    foreach (var buffID in configAction.actionFinishIDList) {
+                        BuffSystem.Instance.AttachBuff(buffID, _skillCreater, _skillCreater, this, null);
+                    }
+                }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
-            else if (movePos.IsOnlyAxis_Y()) {
-                moveType = MoveType.Y;
+        }
+
+        void AddLineAction() {
+            FixIntVector3 movePos = new FixIntVector3(configAction.movePos);
+            FixIntVector3 targetPos = actionActor.LogicPos + movePos * actionActor.LogicAxis_X;
+            FixIntVector3 startPos = actionActor.LogicPos;
+            // Debug.LogError($"add move action pos:{targetPos}");
+            MoveType moveType = MoveType.Target;
+            if (configAction.moveActionType == MoveActionType.TargerPos) {
+                if (movePos.IsOnlyAxis_X()) {
+                    moveType = MoveType.X;
+                }
+                else if (movePos.IsOnlyAxis_Y()) {
+                    moveType = MoveType.Y;
+                }
+                else if (movePos.IsOnlyAxis_Z()) {
+                    moveType = MoveType.Z;
+                }
+                else {
+                    moveType = MoveType.Target;
+                }
             }
-            else if (movePos.IsOnlyAxis_Z()) {
-                moveType = MoveType.Z;
+            else if (configAction.moveActionType == MoveActionType.GuidePos) {
+                targetPos = skillGuidePos;
+                startPos = targetPos + _skillCreater.LogicAxis_X * new FixIntVector3(effectOffset);
+                startPos.y = FixIntMath.Abs(startPos.y);
             }
             else {
-                moveType = MoveType.Target;
+                Debug.LogError($"暂时不支持其他类型的技能特效引导{configAction.moveActionType}");
+            }
+            LogicActionController.Instance.RunAction(new MoveToAction(
+                actionActor,
+                startPos,
+                targetPos,
+                configAction.durationMS,
+                OnActionFinish,
+                onMoveUpdate,
+                moveType
+            ));
+        }
+
+        void AddBezierAction() {
+            if (configAction.moveActionType == MoveActionType.BezierPos) {
+                var startPos = _skillCreater.LogicPos + _skillCreater.LogicAxis_X * new FixIntVector3(effectOffset);
+                startPos.y = FixIntMath.Abs(startPos.y);
+
+                var highestPos = new FixIntVector3(configAction.heightPos) * _skillCreater.LogicAxis_X;
+                highestPos.y = FixIntMath.Abs(highestPos.y);
+                highestPos += _skillCreater.LogicPos;
+
+                var endPos = new FixIntVector3(configAction.movePos) * _skillCreater.LogicAxis_X;
+                endPos.y = FixIntMath.Abs(endPos.y);
+                endPos += _skillCreater.LogicPos;
+                MoveBezierAction bezierAction = new MoveBezierAction(actionActor, startPos, highestPos, endPos, configAction.durationMS, OnActionFinish, onMoveUpdate);
+                LogicActionController.Instance.RunAction(bezierAction);
+            }
+            else {
+                Debug.LogError($"AddBezierAction 暂时不支持其他类型的技能特效引导{configAction.moveActionType}");
             }
         }
-        else if (configAction.moveActionType == MoveActionType.GuidePos) {
-            // 目标位置
-            targetPos = skillGuidePos;
-            // 起始位置
-            startPos = targetPos + _skillCreater.LogicAxis_X * new FixIntVector3(effectOffset);
-            startPos.y = FixIntMath.Abs(startPos.y);
-            // startPos = targetPos + new FixIntVector3(effectOffset);
-            // moveType = MoveType.Target;
-            // 调用MoveToAction
-        }
-        else if (configAction.moveActionType == MoveActionType.BezierPos) {
-            //
-            startPos = _skillCreater.LogicPos + _skillCreater.LogicAxis_X * new FixIntVector3(effectOffset);
-            startPos.y = FixIntMath.Abs(startPos.y);
 
-            //
-            FixIntVector3 highestPos = new FixIntVector3(configAction.heightPos) * _skillCreater.LogicAxis_X;
-            highestPos.y = FixIntMath.Abs(highestPos.y);
-            highestPos += _skillCreater.LogicPos;
-
-            // 
-            FixIntVector3 endPos = new FixIntVector3(configAction.movePos) * _skillCreater.LogicAxis_X;
-            endPos.y = FixIntMath.Abs(endPos.y);
-            targetPos += _skillCreater.LogicPos;
-            
-            MoveBezierAction bezierAction = new MoveBezierAction(,) {
-                
-            }
+        switch (configAction.moveActionType) {
+            case MoveActionType.TargerPos:
+            case MoveActionType.GuidePos:
+                AddLineAction();
+                break;
+            case MoveActionType.BezierPos:
+                AddBezierAction();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
-        else {
-            Debug.LogError($"暂时不支持其他类型的技能特效引导{configAction.moveActionType}");
-        }
-        MoveToAction moveToAction = new(
-            actionActor,
-            startPos,
-            targetPos,
-            configAction.durationMS,
-            () => {
-                onMoveFinish?.Invoke();
-                switch (configAction.moveActionFinishOpation) {
-                    case MoveActionFinishOpation.None:
-                        break;
-                    case MoveActionFinishOpation.Skill: {
-                        foreach (int skillID in configAction.actionFinishIDList) {
-                            _skillCreater.ReleaseSkill(skillID, null);
-                        }
-                    }
-                        break;
-                    case MoveActionFinishOpation.Buff: {
-                        // buff系统
-                        foreach (var buffID in configAction.actionFinishIDList) {
-                            BuffSystem.Instance.AttachBuff(buffID, _skillCreater, _skillCreater, this, null);
-                        }
-                    }
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            },
-            onMoveUpdate,
-            moveType
-        );
-
-        // 开始行动
-        LogicActionController.Instance.RunAction(moveToAction);
     }
 }
