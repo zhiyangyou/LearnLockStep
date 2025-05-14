3c2afbae5948125d7c2b0d247dc1dd03dc3f6e89
youzhiyang <1454900087@qq.com>
2025-05-14 16:36:01 +0800

    NEW: 自动赋值UI的相机组件

---
+++ Unity/Client/Assets/Scripts/Game/Tools/CanvasCamera.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class CanvasCamera : MonoBehaviour {
+    private void Awake() {
+        GetComponent<Canvas>().worldCamera = UIModule.Instance.Camera;
+    }
+}
