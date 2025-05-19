b55163c50c38d18cbe9fad5356abf0b71a53f732
youzhiyang <1454900087@qq.com>
2025-05-19 15:38:19 +0800

    MINOR: 连接服务端

--- Unity/Client/Assets/GameScripts/GameFramework/Main.cs
+++ Unity/Client/Assets/GameScripts/GameFramework/Main.cs
@@ -8,6 +8,7 @@ using ZMGC.Battle;
 using ZMGC.Hall;
 using ZM.ZMAsset;
 using Fantasy;
+using Fantasy.Network;
 using Fantasy.Platform.Unity;
 using FantasyScene = Fantasy.Scene;
 
@@ -37,11 +38,31 @@ public class Main : MonoBehaviour {
 
     #endregion
 
-    #region private
+    #region server
+
+    private const string Server_Gate_Addresss = "127.0.0.1:11002";
 
     private async Task InitServer() {
         await Entry.Initialize();
         _fScene = await Entry.CreateScene();
+        _fScene.Connect(Server_Gate_Addresss,
+            NetworkProtocolType.TCP,
+            OnServerGate_ConnectComplete,
+            OnServerGate_ConnectFailed,
+            OnServer_Gate_Disconnect,
+            false, 5000);
+    }
+
+    private void OnServer_Gate_Disconnect() {
+        Debug.LogError($"{Server_Gate_Addresss} Disconnect");
+    }
+
+    private void OnServerGate_ConnectFailed() {
+        Debug.LogError($"{Server_Gate_Addresss} ConnectFailed");
+    }
+
+    private void OnServerGate_ConnectComplete() {
+        Debug.LogError($"{Server_Gate_Addresss} ConnectComplete");
     }
 
     #endregion
