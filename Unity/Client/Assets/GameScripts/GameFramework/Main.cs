94e9d451cc9426c8372716daf361f10dd1663dca
youzhiyang <1454900087@qq.com>
2025-05-19 11:38:52 +0800

    NEW: 初始化客户端的Fantasy

--- Unity/Client/Assets/GameScripts/GameFramework/Main.cs
+++ Unity/Client/Assets/GameScripts/GameFramework/Main.cs
@@ -1,31 +1,48 @@
 using System;
 using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using ZM.ZMAsset;
 using ZMGC.Battle;
 using ZMGC.Hall;
 using ZM.ZMAsset;
+using Fantasy;
+using Fantasy.Platform.Unity;
+using FantasyScene = Fantasy.Scene;
 
-public class Main : MonoBehaviour
-{
+public class Main : MonoBehaviour {
+    #region 属性字段
+
     private static Main _instance = null;
-
     public static Main Instance => _instance;
 
-    private void Awake() {
+    private FantasyScene _fScene = null;
+
+    #endregion
+
+    #region life-cycle
+
+    private async Task Awake() {
         DontDestroyOnLoad(gameObject);
+        await InitServer();
     }
 
-    void Start()
-    { 
+    void Start() {
         _instance = this;
         UIModule.Instance.Initialize();
         ZMAsset.InitFrameWork();
         WorldManager.CreateWorld<HallWorld>();
     }
 
-    private void Update() {
-       
+    #endregion
+
+    #region private
+
+    private async Task InitServer() {
+        await Entry.Initialize();
+        _fScene = await Entry.CreateScene();
     }
+
+    #endregion
 }
