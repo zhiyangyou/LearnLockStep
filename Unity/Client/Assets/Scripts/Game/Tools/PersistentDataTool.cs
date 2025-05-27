007ce23ec073053ede77c3db996d42a511493f2e
youzhiyang <1454900087@qq.com>
2025-05-27 11:19:39 +0800

    MINOR: 持久化数据微调

--- Unity/Client/Assets/Scripts/Game/Tools/PersistentDataTool.cs
+++ Unity/Client/Assets/Scripts/Game/Tools/PersistentDataTool.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 
 public static class PersistentDataTool {
+    private static string ProcessKey(string key) {
+#if UNITY_EDITOR
+        return $"{Application.dataPath}-{key}";
+#else
+        return key;
+#endif
+    }
+
     public static void SaveStr(string key, string value) {
-        PlayerPrefs.SetString(key, value);
+        PlayerPrefs.SetString(ProcessKey(key), value);
     }
 
     public static string LoadStr(string key) {
-        return PlayerPrefs.GetString(key, "");
+        return PlayerPrefs.GetString(ProcessKey(key), "");
     }
 }
